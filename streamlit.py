8e6cc3b7d4989b7ef9f85572930bb7270af2489f
Rainer Mensing <31634583+rainermensing@users.noreply.github.com>
2022-03-04 15:34:55 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -148,7 +148,7 @@ def main():
         st.info("The selection you made is too narrow. Please remove or change the filter.")
     
     st.write("# About")
-    st.write("This Dashboard was build  by me (Rainer) as Capstone project for the Pipeline Academy Data Engineering Bootcamp. If you want to see how it works, visit the projects GitHub repository\
+    st.write("This Dashboard was build  by me (Rainer) as Capstone project for the Pipeline Academy Data Engineering Bootcamp. If you want to see how it works, visit the projects [GitHub repository](https://github.com/rainermensing/GDLive-Explorer). \
         Also, please note that this is project is still work in progress. I am planning to add new features as I have time. Please feel free to reach out to me at [rainer.mensing@hotmail.de](mailto:rainer.mensing@hotmail.de)")
         
 
