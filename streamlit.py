8eeb9c6aa149ccf43824e2f00c58cf5924aa05df
Rainer <rainer.mensing@hotmail.de>
2022-03-21 22:06:12 +0100

    WordCounter optimized
    Optimized wordcounter performance using some refactoring and parallelization with dask

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,11 @@ nltk.download(['averaged_perceptron_tagger','punkt'])
 import pandas as pd
 import numpy as np
 
+from dask import delayed, compute
+from dask.diagnostics import ProgressBar
+pbar = ProgressBar()
+pbar.register()
+
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
@@ -66,7 +71,7 @@ def cloud(text, max_word, max_font, random):
     plt.axis('off')
     st.pyplot(fig)
 
-def WordCounter(text):
+def WordCounter(text): 
 
 
     #print('PROPER NOUNS EXTRACTED :')
@@ -79,27 +84,36 @@ def WordCounter(text):
     temp = np.char.replace(text,"."," ") # Use instead of nltks slow sent_tokenize()
     temp = np.char.replace(temp,","," ") #remove commas
     temp = np.char.replace(temp,"!"," ") #remove exclamations marks
-    clean_text = np.char.replace(temp,"?"," ") #remove question marks
-    words = np.char.split(clean_text) #split text into single words
+    clean_text = np.char.replace(temp,"?"," ") #remove question marks    
+    words = str.split(str(clean_text)) #split text into single words #FYI using np.char.split here caused an Index Error when trying to split the np array into partitions using np.array_split.
+    print("Text cleaned and split")
 
-    pos_tag_v = np.vectorize(nltk.pos_tag) #creating a numpy version of nltks' tagger funtion
-    tagged = pos_tag_v(words)
+    tasks = []
+    split_array = np.array_split(words,15) #A test on the full dataset showed that 15 parallel dask tasks are optimal.
+    for array in split_array:
+        task = delayed(nltk.pos_tag)(array)
+        tasks.append(task)        
 
-    for (word, tag) in tagged:
-        if tag in ['NN','NNP','NNS']: # If the word is a noun
-            noun_list.append(word)
-        elif tag in ['VB','VBD','VBG','VBN','VBP','VBZ']: # If the word is a verb
-            verb_list.append(word)
-        elif tag in ['JJR','JJS']: # If the word is an adjective
-            adj_list.append(word)    
-    noun_df = pd.DataFrame(noun_list,columns=["Noun"]).groupby(["Noun"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
-    verb_df = pd.DataFrame(verb_list,columns=["Verb"]).groupby(["Verb"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
-    adj_df = pd.DataFrame(adj_list,columns=["Adjective"]).groupby(["Adjective"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
-    
+    dask_product = compute(*tasks)
+     #creating a numpy version of nltks' tagger funtion
+    print("Words tagged")
+    for tagged in dask_product:
+        for (word, tag) in tagged:
+            if tag in ['NN','NNP','NNS']: # If the word is a noun
+                noun_list.append(word)
+            elif tag in ['VB','VBD','VBG','VBN','VBP','VBZ']: # If the word is a verb
+                verb_list.append(word)
+            elif tag in ['JJR','JJS']: # If the word is an adjective
+                adj_list.append(word)    
+        noun_df = pd.DataFrame(noun_list,columns=["Noun"]).groupby(["Noun"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
+        verb_df = pd.DataFrame(verb_list,columns=["Verb"]).groupby(["Verb"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
+        adj_df = pd.DataFrame(adj_list,columns=["Adjective"]).groupby(["Adjective"]).size().reset_index(name='#').sort_values("#",ascending=False).reset_index(drop=True)
+        
     #Let index start from 1 instead of 0
     noun_df.index = noun_df.index + 1
     verb_df.index = verb_df.index + 1
     adj_df.index = adj_df.index + 1
+    print("Wordcount complete")
 
     return noun_df,verb_df,adj_df
 
