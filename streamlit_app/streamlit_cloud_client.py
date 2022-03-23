33868258ed9146697c041b68f2a09283a42dbae7
Rainer <rainer.mensing@hotmail.de>
2022-03-23 23:21:34 +0100

    fixing auth issues take 4

--- streamlit_app/streamlit_cloud_client.py
+++ streamlit_app/streamlit_cloud_client.py
@@ -3,8 +3,11 @@ from google.cloud import bigquery
 import streamlit as st
 
 def get_stcloud_client():
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"]
-    )
-    print("Streamlit Cloud detected: Using Streamlit Cloud access GCP client.")
-    return bigquery.Client(credentials=credentials)
+    try:
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"]
+        )
+        print("Streamlit Cloud detected: Using Streamlit Cloud access GCP client.")
+        return bigquery.Client(credentials=credentials)
+    except:
+        pass
